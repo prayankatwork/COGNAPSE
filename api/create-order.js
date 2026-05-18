6d5e8a93643d0efee3c8b7f46f35a0ca608ea129
Prayank Patnaik <prayankatwork@gmail.com>
2026-05-18 20:06:07 +0530

    fix: resolve es module require errors in api endpoints and fix media CSP

--- api/create-order.js
+++ api/create-order.js
@@ -1,6 +1,6 @@
-const Razorpay = require('razorpay');
+import Razorpay from 'razorpay';
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
